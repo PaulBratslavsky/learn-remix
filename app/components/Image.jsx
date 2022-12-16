e69a12cd183112308d05b69f3681d0e927eba09d
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2022-12-16 16:57:59 -0600

    image optimization (#2)
    Oh boy

--- app/components/Image.jsx
+++ app/components/Image.jsx
@@ -1,3 +1,13 @@
+import {Cloudinary} from "@cloudinary/url-gen";
+import {AdvancedImage, responsive} from '@cloudinary/react';
+
+ // Create and configure your Cloudinary instance.
+ const cld = new Cloudinary({
+  cloud: { cloudName: 'dq2cllwgp' }
+}); 
+
+
+
 export default function Image({ data }) {
   const {
     title,
@@ -10,19 +20,24 @@ export default function Image({ data }) {
     image,
   } = data;
 
+  const myImage = cld.image(image.data.attributes.provider_metadata.public_id);
+
+  console.log(myImage, "myImage");
 // TODO: FIX PASSING SHOWTITLE AND SHOWDESCRIPTION FROM STRAPI
   return (
     <div className="my-6">
       {title && showTitle && <h2 className="text-2xl mb-3">Title: {title}</h2>}
 
       {image.data !== null && (
-        <img
-          className="rounded-xl"
-          src={data.image.data.attributes.url}
-          alt={alt || "No alt text was provided"}
-          height={height}
-          width={width}
-        />
+        // <img
+        //   className="rounded-xl"
+        //   src={data.image.data.attributes.url}
+        //   alt={alt || "No alt text was provided"}
+        //   height={height}
+        //   width={width}
+        // />
+        <AdvancedImage  className="rounded-xl" cldImg={myImage} plugins={[responsive({steps: 200})]}/>
+
       )}
 
       {description && showDescription && (
